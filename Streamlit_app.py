e5d358997634589fae81af429d0aa42b38123ba5
LucieRC <lricqpro@outlook.fr>
2024-07-02 14:40:59 +0200

    Updated deputees table & created app

---
+++ Streamlit_app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+# Title of the app
+st.title("Two Columns Table Example")
+
+# # Data for the table
+# data = {
+#     "Column 1": ["Row 1", "Row 2", "Row 3"],
+#     "Column 2": ["Data 1", "Data 2", "Data 3"]
+# }
+
+# # Create a DataFrame
+# df = pd.DataFrame(data)
+
+# Load the CSV file
+csv_file = "input/target_deputees.csv"
+df = pd.read_csv(csv_file)
+
+# Display the table
+st.table(df)
